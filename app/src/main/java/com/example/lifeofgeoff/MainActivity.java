86c619ef2caa7da4326afa0781e0e745d135a1c6
Maanav Patel <maanav.patel18@icloud.com>
2019-12-02 17:13:41 -0600

    adding code and ui ids

--- app/src/main/java/com/example/lifeofgeoff/MainActivity.java
+++ app/src/main/java/com/example/lifeofgeoff/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lifeofgeoff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onClick();
+        Button b = findViewById(R.id.lifeWithGeoff);
+        View.OnClickListener j = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+        b.setOnClickListener(j);
     }
 }
