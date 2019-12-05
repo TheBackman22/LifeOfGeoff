77fa6e172950b5715e960bd2a9b81abfdd416d0d
Maanav Patel <maanav.patel18@icloud.com>
2019-12-04 18:44:08 -0600

    camera stuff

--- app/src/main/java/com/example/lifeofgeoff/MainActivity.java
+++ app/src/main/java/com/example/lifeofgeoff/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.lifeofgeoff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         View.OnClickListener lifeWith = new View.OnClickListener() {
             @Override
@@ -21,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("life with geoff selected");
                 Intent camera = new Intent(view.getContext(), CameraActivity.class);
                 startActivity(camera);
-                finish();
             }
         };
         Button lifeWG = findViewById(R.id.lifeWithGeoff);
